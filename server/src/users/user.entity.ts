c8157dd1663934a1c57a39b65b2a6fcb2df758e1
Ilya Onishchik <ilyaonishchik@gmail.com>
2024-05-08 18:44:19 +0300

    add user entity

---
+++ server/src/users/user.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  @UpdateDateColumn()
+  updatedAt: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  passwordHash: string;
+
+  @Column({ unique: true })
+  verificationLink: string;
+
+  @Column({ default: false })
+  verified: boolean;
+}
