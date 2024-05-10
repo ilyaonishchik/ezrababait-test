8666ced0e92238c58a0888f15488656576dc3248
Ilya Onishchik <ilyaonishchik@gmail.com>
2024-05-10 14:55:17 +0300

    configure cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: process.env.CLIENT_URL, credentials: true });
   app.use(cookieParser());
   await app.listen(3000);
 }
