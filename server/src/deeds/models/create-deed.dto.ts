af347cdc131eb3de209712e04935600f9fe45635
Ilya Onishchik <ilyaonishchik@gmail.com>
2024-05-11 19:03:56 +0300

    implement class-validator

--- server/src/deeds/models/create-deed.dto.ts
+++ server/src/deeds/models/create-deed.dto.ts
@@ -1,5 +1,16 @@
+import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
+
 export class CreateDeedDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   description: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(1)
   points: number;
 }
--- server/src/deeds/models/update-deed.dto.ts
+++ server/src/deeds/models/update-deed.dto.ts
@@ -1,6 +1,15 @@
+import { IsBoolean, IsNumber, IsString } from 'class-validator';
+
 export class UpdateDeedDto {
+  @IsString()
   title?: string;
+
+  @IsString()
   description?: string;
+
+  @IsNumber()
   points?: number;
+
+  @IsBoolean()
   completed?: boolean;
 }
